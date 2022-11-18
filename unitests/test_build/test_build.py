23efce32d25e58a42dd2bcd62e2c16b355d2b27b
Dexter X1n <chx1n.d@gmail.com>
2022-11-18 19:39:45 +0800

    v0.3: Update test_build for last ArgCat commit.

--- unitests/test_build/test_build.py
+++ unitests/test_build/test_build.py
@@ -14,21 +14,21 @@ class TestBuild(ArgCatUnitTest):
             #2. add_argument(option_string, option_string, ..., name=value, ...)
         
             # Add argument to the main parser
-            builder.add_argument('main', 'verbose', action='store_true')
+            builder.new_argument(ignored_by_subparser=True).add('verbose', action='store_true')
             #builder.add_argument('main', 'foo', '-o', '--fooa', action='store_false')  # This is a wrong usage.
             #builder.add_argument('main', '-v', '--verbose', action='store_true')
             # Add group for the sub parser
-            builder.add_group('process', 'process_group_1', 
+            builder.add_group(parser_name='process', group_name='process_group_1', 
                               description='This is the first group for process, which is not mutually exclusive', 
                               is_mutually_exclusive=False)
-            builder.add_group('process', 'process_group_2', 
+            builder.add_group(parser_name='process', group_name='process_group_2', 
                               description='This is the second group for process, which IS mutually exclusive', 
                               is_mutually_exclusive=True)
             # Add argument to sub parsers
-            builder.add_argument('process', '-f', '--file', 
+            builder.new_argument('process').add('-f', '--file', 
                                  nargs='?', dest='filename', type='str', group='process_group_1', required=True,
                                  help='The target file name.')
-            builder.add_argument('process', '-s', '--size', nargs='1', dest='filesize', type='int', 
+            builder.new_argument('process').add('-s', '--size', nargs='1', dest='filesize', type='int', 
                                  group='process_group_1', required=False, default='1024')
             
 
@@ -80,8 +80,10 @@ class TestBuild(ArgCatUnitTest):
         self.assertEqual(filename_arg.metavar, None, "The `metavar` of `filename` argument is wrong!")
         self.assertEqual(filename_arg.help, "The target file name.", "The `help` of `filename` argument is wrong!")
         self.assertEqual(filename_arg.required, True, "The `required` of `filename` argument is wrong!")
-        self.assertEqual(filename_arg.group_name, 'process_group_1', "The `group` of `filename` argument is wrong!")
         self.assertEqual(filename_arg.default, None, "The `default` of `filename` argument should be None!")
+        
+        group_name = process_parser.additional_arguments_info[filename_arg.dest]['group']
+        self.assertEqual(group_name, 'process_group_1', "The `group` of `filename` argument is wrong!")
         
         # Check the second argument of `process` parser: filesize
         second_matches = [arg for arg in process_parser.arguments if arg.dest == 'filesize']
@@ -97,9 +99,15 @@ class TestBuild(ArgCatUnitTest):
         self.assertEqual(filesize_arg.metavar, None, "The `metavar` of `filesize` argument is wrong!")
         self.assertEqual(filesize_arg.default, '1024', "The `default` of `filesize` is incorrect!")
         self.assertEqual(filesize_arg.required, False, "The `required` of `filesize` argument is wrong!")
-        self.assertEqual(filesize_arg.group_name, 'process_group_1', "The `group_name` of `filesize` argument is wrong!")
         
         # Test groups
+        
+        group_name = process_parser.additional_arguments_info[filename_arg.dest]['group']
+        self.assertEqual(group_name, 'process_group_1', "The `group` of `filename` argument is wrong!")
+        
+        group_name = process_parser.additional_arguments_info[filesize_arg.dest]['group']
+        self.assertEqual(group_name, 'process_group_1', "The `group` of `filename` argument is wrong!")
+        
         self.assertEqual(len(process_parser.groups), 2, "The number of groups for `process` parser is wrong!")
         
         process_group_1 = process_parser.groups['process_group_1']
